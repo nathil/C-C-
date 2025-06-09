fc449928fe0bc77807c29bd8abc90d9db2d78977
Nathielly Castro <134755228+nathil@users.noreply.github.com>
2025-06-09 17:13:53 +0000

    add

---
+++ questao05.c
@@ -0,0 +1,17 @@
+//Ler um número inteiro e apresentar: NUMERO=xx, QUADRADO=xx e RAIZ QUADRADA=xx
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int valor, quadrado, raiz; 
+
+    printf("informe um valor:"); 
+    scanf("%d", &valor);
+
+    quadrado = pow(valor,2); 
+    raiz = sqrt(valor);
+
+    printf("número: %d\nquadrado: %d\nraiz: %d\n", valor, quadrado, raiz); 
+}
